389f97beb561240cddc052a4661d26e6598a55bf
German147 <barreragerman27@gmail.com>
2021-09-25 10:56:02 -0300

    It was been created PasswordEncoder class and encode each password of the inMemory users

--- src/main/java/proyecto/escuela/escalab/ProyectoEscuelaEscalab/security/ApplicationSecurityConfig.java
+++ src/main/java/proyecto/escuela/escalab/ProyectoEscuelaEscalab/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package proyecto.escuela.escalab.ProyectoEscuelaEscalab.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,12 +9,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -32,39 +40,40 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected UserDetailsService userDetailsService() {
         UserDetails lucasUser = User.builder()
                 .username("lucas")
-                .password("password")
+                .password(passwordEncoder.encode("password"))
                 .roles("STUDENT")//ROLE_STUDENT
                 .build();
 
         UserDetails evangeUser = User.builder()
                 .username("evange")
-                .password("password")
+                .password(passwordEncoder.encode("password"))
                 .roles("TEACHER")//ROLE_TEACHER
                 .build();
 
         UserDetails carlosUser = User.builder()
                 .username("carlos")
-                .password("password")
-                .roles("APODERADO")//ROLE_STUDENT
+                .password(passwordEncoder.encode("password"))
+                .roles("APODERADO")//ROLE_APODERADO
                 .build();
+
         UserDetails claudiaUser = User.builder()
                 .username("claudia")
-                .password("password")
+                .password(passwordEncoder.encode("pasword"))
                 .roles("PRINCIPAL")//ROLE_STUDENT
                 .build();
         UserDetails paolaUser = User.builder()
                 .username("paola")
-                .password("password")
+                .password(passwordEncoder.encode("pasword"))
                 .roles("SECRETARY")//ROLE_STUDENT
                 .build();
         UserDetails germanUser = User.builder()
                 .username("german")
-                .password("password")
+                .password(passwordEncoder.encode("pasword"))
                 .roles("ADMIN")//ROLE_STUDENT
                 .build();
         UserDetails maricelUser = User.builder()
                 .username("maricel")
-                .password("password")
+                .password(passwordEncoder.encode("pasword"))
                 .roles("PRECEPTOR")//ROLE_STUDENT
                 .build();
         return new InMemoryUserDetailsManager(
