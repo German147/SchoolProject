29097b27404de052f47918b060a2d7839f75bb26
German147 <barreragerman27@gmail.com>
2021-09-25 10:38:48 -0300

    It was been created  In Memory UserDetailsManager, here some users were created

--- src/main/java/proyecto/escuela/escalab/ProyectoEscuelaEscalab/security/ApplicationSecurityConfig.java
+++ src/main/java/proyecto/escuela/escalab/ProyectoEscuelaEscalab/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package proyecto.escuela.escalab.ProyectoEscuelaEscalab.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -20,5 +25,55 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails lucasUser = User.builder()
+                .username("lucas")
+                .password("password")
+                .roles("STUDENT")//ROLE_STUDENT
+                .build();
+
+        UserDetails evangeUser = User.builder()
+                .username("evange")
+                .password("password")
+                .roles("TEACHER")//ROLE_TEACHER
+                .build();
+
+        UserDetails carlosUser = User.builder()
+                .username("carlos")
+                .password("password")
+                .roles("APODERADO")//ROLE_STUDENT
+                .build();
+        UserDetails claudiaUser = User.builder()
+                .username("claudia")
+                .password("password")
+                .roles("PRINCIPAL")//ROLE_STUDENT
+                .build();
+        UserDetails paolaUser = User.builder()
+                .username("paola")
+                .password("password")
+                .roles("SECRETARY")//ROLE_STUDENT
+                .build();
+        UserDetails germanUser = User.builder()
+                .username("german")
+                .password("password")
+                .roles("ADMIN")//ROLE_STUDENT
+                .build();
+        UserDetails maricelUser = User.builder()
+                .username("maricel")
+                .password("password")
+                .roles("PRECEPTOR")//ROLE_STUDENT
+                .build();
+        return new InMemoryUserDetailsManager(
+                lucasUser,
+                evangeUser,
+                carlosUser,
+                claudiaUser,
+                germanUser,
+                maricelUser
+        );
     }
 }
