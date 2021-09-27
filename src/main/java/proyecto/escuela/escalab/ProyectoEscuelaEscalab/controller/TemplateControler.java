a98396ebb57a5e7458215131b8fce7215fb65205
German147 <barreragerman27@gmail.com>
2021-09-27 17:11:03 -0300

    It was Implemented  Form based Authentication into the ApplicationSecurityConfig, it was also created the .html files of login.html, school.html and the TemplateControler. It was addd to the pom.xml Thymeleaf dependency

---
+++ src/main/java/proyecto/escuela/escalab/ProyectoEscuelaEscalab/controller/TemplateControler.java
@@ -0,0 +1,18 @@
+package proyecto.escuela.escalab.ProyectoEscuelaEscalab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class TemplateControler {
+    @GetMapping("login")
+    public String getLoginView(){
+        return "login";
+    }
+    @GetMapping("school")
+    public String getSchoolView(){
+        return "school";
+    }
+}
--- src/main/java/proyecto/escuela/escalab/ProyectoEscuelaEscalab/security/ApplicationSecurityConfig.java
+++ src/main/java/proyecto/escuela/escalab/ProyectoEscuelaEscalab/security/ApplicationSecurityConfig.java
@@ -13,6 +13,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static proyecto.escuela.escalab.ProyectoEscuelaEscalab.security.AplicationUserRole.*;
 
@@ -31,23 +34,43 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                // .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "js/*").permitAll()//this anotation alows everybody to get in the login page
                 // .antMatchers("/api/**").hasRole(ADMIN.name())
+                //here it is set the ROLE to the the endpoint, and in each Controller it is Configuresd the @PreAuthorize() anotation...
                 .antMatchers(HttpMethod.GET, "/api/v1/alumnos").hasAnyRole(ADMIN.name(), PROFESOR.name(), PRECEPTOR.name())
                 .antMatchers(HttpMethod.GET, "/api/v1/apoderados").hasAnyRole(ADMIN.name(), PRECEPTOR.name(), SECRETARIA.name())
                 .antMatchers(HttpMethod.GET, "/api/v1/asignaturas").hasAnyRole(ADMIN.name(), PROFESOR.name(), PRECEPTOR.name())
                 .antMatchers(HttpMethod.GET, "/api/v1/contenidos").hasAnyRole(ADMIN.name(), PROFESOR.name(), ALUMNO.name())
-                .antMatchers(HttpMethod.GET,"/api/v1/cursos").hasAnyRole(ADMIN.name(), PROFESOR.name(), PRECEPTOR.name())
-                .antMatchers(HttpMethod.GET,"/api/v1/ficha_medica").hasAnyRole(ADMIN.name(), PRECEPTOR.name(), SECRETARIA.name())
-                .antMatchers(HttpMethod.GET,"/api/v1/profesores").hasAnyRole(ADMIN.name(), PRECEPTOR.name(), SECRETARIA.name(), ALUMNO.name())
-                .antMatchers(HttpMethod.GET,"/api/v1/registro_academico").hasAnyRole(ADMIN.name(), PRECEPTOR.name(), APODERADO.name(), SECRETARIA.name())
-                .antMatchers(HttpMethod.GET,"/api/v1/toma_asignaturas").hasAnyRole(ADMIN.name(), PROFESOR.name(), SECRETARIA.name())
+                .antMatchers(HttpMethod.GET, "/api/v1/cursos").hasAnyRole(ADMIN.name(), PROFESOR.name(), PRECEPTOR.name())
+                .antMatchers(HttpMethod.GET, "/api/v1/ficha_medica").hasAnyRole(ADMIN.name(), PRECEPTOR.name(), SECRETARIA.name())
+                .antMatchers(HttpMethod.GET, "/api/v1/profesores").hasAnyRole(ADMIN.name(), PRECEPTOR.name(), SECRETARIA.name(), ALUMNO.name())
+                .antMatchers(HttpMethod.GET, "/api/v1/registro_academico").hasAnyRole(ADMIN.name(), PRECEPTOR.name(), APODERADO.name(), SECRETARIA.name())
+                .antMatchers(HttpMethod.GET, "/api/v1/toma_asignaturas").hasAnyRole(ADMIN.name(), PROFESOR.name(), SECRETARIA.name())
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                    .loginPage("/login").permitAll()
+                    .defaultSuccessUrl("/school",true)
+                    .passwordParameter("password")//this anotation has to be the same as the login form parameter
+                    .usernameParameter("username")//this anotation has to be the same as the login form parameter
+                    .and()
+                    .rememberMe()
+                    .rememberMeParameter("remember-me")
+                .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))//it by default for 2 weeks
+                    .key("strongpasswordmustbeset")
+                    .and()
+                .logout()
+                    .logoutUrl("/logout")
+                //we use GET to loogout when we have desable CSRF,but if not it must be a POST
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("remember-me","JSESSIONID")
+                    .logoutSuccessUrl("/login");
     }
 
     @Override
